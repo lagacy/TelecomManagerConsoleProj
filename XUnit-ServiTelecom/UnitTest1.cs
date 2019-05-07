ff5b1f924057508a79fe9b880c68ca8bfab4abce
Vincent Roy <lagacy2@hotmail.com>
2019-05-06 21:31:27 -0400

    Minor changes.
    Finish preparing the Unit test project, but for the sake of praticing the langage and core library i'm gonna drop the TDD approach.

--- XUnit-ServiTelecom/UnitTest1.cs
+++ XUnit-ServiTelecom/UnitTest1.cs
@@ -2,6 +2,17 @@ using System;
 using Xunit;
 using ServiTelecom;
 
+/*
+ *Here is the base convention for test naming:
+ * [the name of the tested method]_[expected input / tested state]_[expected behavior]
+ */
+
+    /*Structure of a unit test
+     * Arrange
+     * Act
+     * Assert
+     */
+
 namespace XUnit_ServiTelecom
 {
     public class UnitTest1
